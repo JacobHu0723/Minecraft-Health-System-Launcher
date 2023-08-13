e612a8621cc15a9da480acd80ef4a7babbf03df6
JacobHu0723 <63916353+JacobHu0723@users.noreply.github.com>
2023-08-13 12:00:43 +0800

    Update main.py
    Fixed a bug that could not monitor the time.

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ i.start()
 os.startfile(path)
 while True:
     # 检查是否正在运行
-    if is_process_running('javaw.exe'):
+    if is_process_running('java.exe'):
         pass
         # print(f"{elapsed_time} / {limit}")
     else:
